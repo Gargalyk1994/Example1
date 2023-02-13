43200a3701f1d1a996b91d0c60f6c40884e76170
Anton Gargalyk <gargalyk1994@gmail.com>
2023-02-13 13:10:02 +0900

    correct Div 2 number

--- Examples004_Div/Program.cs
+++ Examples004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 14;
-double numberB = 65;
+﻿double numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 double result = numberA / numberB;
 Console.WriteLine(result);
